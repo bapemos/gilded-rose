d7a31b6ccb27d1916ccead66c6b7ebb888d2ded1
Brady Pejerrey <bapemos@gmail.com>
2019-03-26 23:29:28 -0400

    - update_quality() basic refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,57 +1,65 @@
 class GildedRose
 
   def initialize(items)
-    @items = items
+    @items = items.collect{|i| ItemOp.new(i)}
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    @items.each { |item| item.update_quality! }
   end
 end
+
+class ItemOp
+  LEGENDARY_ARRAY = ['Sulfuras, Hand of Ragnaros']
+  WELL_AGED_ARRAY = ['Aged Brie']
+  BACKSTAGE_ARRAY = ['Backstage passes to a TAFKAL80ETC concert']
+  MAX_QUALITY = 50
+
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+    #----
+    @type = 'normal'
+    @type = 'legendary' if LEGENDARY_ARRAY.include?(item.name)
+    @type = 'well_aged' if WELL_AGED_ARRAY.include?(item.name)
+    @type = 'backstage' if BACKSTAGE_ARRAY.include?(item.name)
+    #-----
+  end
+
+  def update_quality!
+    return true if @type == 'legendary'
+
+    qlty_taken = 0
+    si_taken = 1
+
+    if @type == 'normal'
+      qlty_taken = 1
+      qlty_taken = 2 if @item.sell_in < 0
+    end
+
+
+    if @type == 'well_aged'
+      qlty_taken = -1
+    end
+
+    if @type == 'backstage'
+      qlty_taken = -1
+      qlty_taken = -2 if @item.sell_in < 11
+      qlty_taken = -3 if @item.sell_in < 6
+      qlty_taken = @item.quality if @item.sell_in < 0
+    end
+
+    qlty_taken = 0 if @item.quality.zero? and qlty_taken > 0
+    qlty_taken = 0 if @item.quality == MAX_QUALITY and qlty_taken < 0
+
+    @item.quality = @item.quality - qlty_taken
+    @item.sell_in = @item.sell_in - si_taken
+
+  end
+
+end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,8 +6,121 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
+
+
+    context "normal items" do
+      it "decreases qlty by 1" do
+        items = [Item.new("Normal item 1", 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 39
+      end
+
+      it "decreases sell_in by 1" do
+        items = [Item.new("Normal item 1", 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 29
+      end
+
+      it "does not decrease quality if zero" do
+        items = [Item.new("Normal item 1", 30, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+    end
+
+    context "well-aging items" do
+      it "increases qlty by 1" do
+        items = [Item.new("Aged Brie", 30, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it "decreases sell_in by 1" do
+        items = [Item.new("Aged Brie", 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 29
+      end
+
+      it "does not increase quality if 50" do
+        items = [Item.new("Aged Brie", 30, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    context "backstage items" do
+      it "increases qlty by 1" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+      end
+
+      it "decreases sell_in by 1" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 29
+      end
+
+      it "does not increase quality if 50" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    context "backstage items with less 10 days" do
+      it "increases qlty by 2" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 2
+      end
+
+      it "decreases sell_in by 1" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 29
+      end
+
+      it "does not increase quality if 50" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    context "backstage items with less 6 days" do
+      it "increases qlty by 3" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 3
+      end
+
+      it "decreases sell_in by 1" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 29
+      end
+
+      it "does not increase quality if 50" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 30, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    context "backstage items expired" do
+      it "drops qlity to zero" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 40)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+
+    end
+
+
+
   end
 
 end
